ad2338ddecbe2bc05b901b72e71316ba86544266
Alex Skorulis <skorulis@gmail.com>
2016-11-15 10:12:52 +1100

    Fix Nada mobile

--- sites/nada.js
+++ sites/nada.js
@@ -1,1 +1,10 @@
-readAndSaveSimpleFloat("NADApoints",".plainpoint");
+function readBalance() {
+    var balance = $('.user--balance .money');
+    if(balance.length > 0) {
+        var text = balance.text().replace(" pts","");
+        text = text.replace(",","");
+        saveBalance("nada",parseInt(text));
+    }
+}
+
+setTimeout(function() { readBalance(); }, 5000);
