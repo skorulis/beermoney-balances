46aff1e02358e13c2360449a2873385d591cb2bf
Alex Skorulis <skorulis@gmail.com>
2017-06-15 19:42:38 +0200

    Update perk tv

--- sites/perktv.js
+++ sites/perktv.js
@@ -1,19 +1,12 @@
 function readPoints() {
-	var outer = $("#total_points");
-	if (outer.length > 0) {
-		var total = "";
-		outer.find(".odometer-value").each(function() {
-			total += $(this).text();
-		});
-		var value = parseInt(total);
-		if(value < 10000000) { //Prevent bad balance reads
-			saveBalance("perktv",value);	
-		} else {
-			setTimeout(function() { readPoints(); }, 5000);			
-		}
+	var element = $(".points-count");
+	if (element.length == 1) {
+		var text = element.text()
+		saveBalance("perktv",parseFloat(text));
 	} else {
 		setTimeout(function() { readPoints(); }, 5000);
 	}
+
 }
 
 setTimeout(function() { readPoints(); }, 5000);
